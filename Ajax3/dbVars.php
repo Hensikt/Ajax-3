57fb08c20a2b60a69fa0156528ec42ced611ed1f
Hensikt <42713950+Hensikt@users.noreply.github.com>
2019-04-10 17:10:53 +0200

    Add files via upload

---
+++ Ajax3/dbVars.php
@@ -0,0 +1,14 @@
+<?php
+
+$servername = "127.0.0.1";
+$uid = "c3655Hensikt";
+$pwd = "#AB2sqZpaXqS";
+$database = "c3655Ajax3";
+
+// QFUeCWsz@yf52
+/* 27728 Bjorn Emmaneel MD1B*/
+// $servername = "localhost";
+// $uid = "root";
+// $pwd = "";
+// $database = "Ajax3";
+?>
